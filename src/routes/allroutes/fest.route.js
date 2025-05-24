5ce1a9d5549d52601271711a1a002e7a9485083b
Pranesh <pranesh25joshi@gmail.com>
2025-05-24 12:08:53 +0530

    fix: correct route handler for fetching fests

--- src/routes/allroutes/fest.route.js
+++ src/routes/allroutes/fest.route.js
@@ -6,7 +6,7 @@ import festController from "../../controllers/fest.controller.js";
 const router = express.Router();
 
 
-router.get("/get-fests", asyncHandler(festController.getFest));
+router.get("/get-fests", asyncHandler(festController.getFests));
 router.post("/add-fest", asyncHandler(festController.addFest));
 router.put("/edit-fest/:id", asyncHandler(festController.editFest));
 router.get("/get-fest/:id", asyncHandler(festController.getFestById));
