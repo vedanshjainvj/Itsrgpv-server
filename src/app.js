081afff326bb350a94862bdf378b5521213d158d
Shreyash Jain <148464006+shreyash-sanghi@users.noreply.github.com>
2025-05-29 21:24:54 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(urlencoded({extended:true}))
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', req.headers.origin); // Allow dynamic origin
-    res.header('Access-Control-Allow-Credentials', 'true');
+    // res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, withCredentials');
     next();
